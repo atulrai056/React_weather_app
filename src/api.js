a2e4c0549252ec16e669a13f4e2dbc9c9220dd3f
Atul Rai <atulrai056@gmail.com>
2023-03-09 19:12:00 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5140d914a8msh4ddc341cd6839a2p1fb47cjsnb723bf1f9dac',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "3a1c7386e85750e2e0aa6f388aed5ce1";
